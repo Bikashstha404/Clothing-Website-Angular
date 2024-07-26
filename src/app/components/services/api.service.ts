461d4c3e8273251e2f2addbb3137456b9f5c24c8
Bikash Shrestha <stha.bikam99@gmail.com>
2024-07-27 01:48:30 +0545

    changed apiService to authService

--- src/app/components/services/api.service.ts
+++ src/app/components/services/api.service.ts
@@ -1,22 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class AuthService {
 
   private baseUrl: string = "http://localhost:5143/api/Auth/";
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
 
   }
 
-  signUp(signUpObj: any){
+  signUp(signUpObj: any):Observable<any>{
     return this.http.post<any>(`${this.baseUrl}SignUp`, signUpObj);
   }
 
-  login(loginObj: any){
+  login(loginObj: any):Observable<any>{
     return this.http.post<any>(`${this.baseUrl}Login`, loginObj);
+  }
+
+  storeToken(tokenValue: string){
+    localStorage.setItem('token', tokenValue)
+  }
+
+  getToken(): string | null{
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean{
+    return !!localStorage.getItem('token');
+  }
+
+  signOut(){
+    localStorage.clear();
+    // localStorage.removeItem('token');
+    this.router.navigate(['login']);
   }
 }
---
+++ src/app/components/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  
+  private baseUrl = "http://localhost:5143/api/User/";
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getAllUsers(): Observable<any>{
+    return this.http.get<any>(this.baseUrl);
+  }
+
+}
